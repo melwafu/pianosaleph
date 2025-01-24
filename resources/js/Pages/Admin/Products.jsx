b2222aecb0c3b9c1a5a1c03cfbaeb0f81b193219
Romel <morisseymorante@firstcom.com.sg>
2025-01-24 20:53:19 +0800

    Modified file: Products.jsx

--- resources/js/Pages/Admin/Products.jsx
+++ resources/js/Pages/Admin/Products.jsx
@@ -195,7 +195,7 @@ function Children(products, category) {
                                                 name="name" 
                                                 id="name" 
                                                 className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                placeholder="Type category name"
+                                                placeholder="Type product name"
                                                 value={data.name}
                                                 onChange={(e) => setData("name", e.target.value)}
                                             />
